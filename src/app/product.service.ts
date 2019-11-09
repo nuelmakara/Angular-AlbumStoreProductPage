db1bff151089108e9c65114aadbd3b9a0352a318
Nuel Makara <nuelmakara@gmail.com>
2019-11-08 19:06:54 -0800

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,10 +8,15 @@ import {Album} from './album';
 
 import {Observable} from 'rxjs/Observable';
 
+import { jsonpFactory } from '@angular/http/src/http_module';
+
+import { Product } from './product';
+
 @Injectable()
 export class ProductService {
 
     private _albumUrl = '../assets/album.json';
+    private _productsUrl = '/assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -19,6 +24,11 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response) => 
     <Album>response.json());
 
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response =>
+      <Product[]>response.json());
   }
 
 }
